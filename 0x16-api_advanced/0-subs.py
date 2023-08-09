9b2465aba7a9e10e1a7b4b1adc86ab8694d270ec
njagi254 <samuelkamau09@gmail.com>
2023-08-09 16:15:31 +0300

    tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
-        "User-Agent": "0x16. API_advanced-njagi254"
+        'User-Agent': '0-subs.py/1.0 (by /u/override254)'
         }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
